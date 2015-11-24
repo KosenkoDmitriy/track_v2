13999210ba402bf07b98a0b00f0aecb17c9d0755
dmitry <kosenkodmitryv@gmail.com>
2015-11-24 17:08:43 +0300

    Fixed routes for spree and alchemy. Added elements: product and product_category. Note:  When try to login to spree admin panel we have the error: "Invalid %{authentication_keys} or password." (http://localhost:3000/shop/login).

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  mount Alchemy::Engine => '/'
+  mount Spree::Core::Engine, :at => '/shop'
+
+  # mount Alchemy::Engine, at: '/'
+  mount Alchemy::Engine => '/', :as => 'alchemy'
 
   # This line mounts Spree's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/shop'
+
+  # mount Spree::Core::Engine, at: '/shop'
+  Spree::Core::Engine.draw_routes
+
   # This line mounts Spree's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
