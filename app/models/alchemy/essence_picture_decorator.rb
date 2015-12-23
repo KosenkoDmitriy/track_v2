ef92749cd4b2f9d2fbea2202439d71231de6e4a9
dmitry <kosenkodmitryv@gmail.com>
2015-12-23 14:15:48 +0300

    Displaying Image on home page, uploaded in the backend (alchemy cms).

--- app/models/alchemy/essence_picture_decorator.rb
+++ app/models/alchemy/essence_picture_decorator.rb
@@ -4,7 +4,8 @@ Alchemy::EssencePicture.class_eval do
     def picture_url(options = {})
       return if picture.nil?
        params = picture_params(options)
-       return picture.image_file.remote_url(host: "#{CLOUDFRONT_CONFIG['image_domain']}.#{CLOUDFRONT_CONFIG['host']}") if params[:crop].nil?
+       # return request.host + picture.image_file_uid
+       # return picture.image_file.remote_url(host: "#{CLOUDFRONT_CONFIG['image_domain']}.#{CLOUDFRONT_CONFIG['host']}") if params[:crop].nil?
        routes.show_picture_path(params)
     end
 end
